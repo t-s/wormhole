92ad5a02e2b1da4941bdca694285fb4ab1666f1b
Tim Sidie <timothy.a.sidie@nasa.gov>
2013-02-07 20:45:06 +0000

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Wormhole
+=========
+When a file appears in a specified director(ies), SCP that file to specfied remote host(s).
+Wormhole uses inotify code from http://code.activestate.com/recipes/576375-low-level-inotify-wrapper/.
+Planned to be expanded with flexible syntax.
---
+++ wormhole.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+"""
+wormhole: scp's a file when file appears in directory(s) to remote host(s)
+
+syntax: wormhole.py -d [directories] -r [remote hosts]
+        wormhole.py -h to print this message
+
+example: wormhole.py /home/user/dir1 ../dir2/ user@remote remote2
+"""
+
+import sys
+import os
+from inotify import Inotify, FLAGS, mask_str
+
+if __name__ == '__main__':
+
+	dirList = []
+	remoteList = []
+	wdDirDict = {}	
+
+	dirs = False
+	remotes = False
+
+	inotify = Inotify()
+
+	sys.argv.pop(0)
+	
+	for arg in sys.argv:
+		if arg == "-h":
+			print __doc__
+		elif arg == '-d':
+			remotes = False
+			dirs = True
+		elif arg == '-r':
+			remotes = True
+			dirs = False
+		elif remotes:
+			remoteList.append(arg)
+		elif dirs:
+			dirList.append(arg)
+
+	for d in dirList:
+		iwd = inotify.add_watch(d,0x00000180)
+		wdDirDict[iwd] = d
+	
+	while True:
+		try:
+			for wd,mask,cookie,name in inotify.read():
+
+				dirn = wdDirDict[wd]
+
+				if mask == 256:
+					print 'File ' + name + ' added in directory'\
+					' ' + dirn + '.'
+				if mask == 128:
+					print 'File ' + name + ' moved to directory'\
+					' ' + dirn + '.'
+
+				for r in remoteList:
+					os.system('scp "%s" "%s:%s"' % (str(dirn+name),r,"/tmp"))
+
+		except KeyboardInterrupt:
+			for iwd in wdDirDict.keys():
+				inotify.rm_watch(iwd)
+			inotify.close()
+			quit()
+				
