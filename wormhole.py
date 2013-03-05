98056ce159bc9fa9f3523e9eeadfd6987ad1fc08
Tim Sidie <timothy.a.sidie@nasa.gov>
2013-03-05 14:45:31 +0000

    Add user argument, fix 0 arguments

--- wormhole.py
+++ wormhole.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-
 """
 wormhole: scp's a file when file appears in directory(s) to remote host(s)
 
-syntax: wormhole.py -d [directories] -r [remote hosts]
-		wormhole.py -c [command] 
+syntax: wormhole.py -d {directories} -r {remote_hosts} -u [username]
+		wormhole.py -c {command}
         wormhole.py -h to print this message
 
-example: wormhole.py /home/user/dir1/ ../dir2/ user@remote1 user@remote2
+example: wormhole.py -d /home/user1/dir1/ ../dir2/ -r remote1 remote2 -u user1
 example: wormhole.py -c sed -i 's/cat/dog/g'
 """
 
 import sys
 import os
+import getpass
 from inotify import Inotify, FLAGS, mask_str
 
 if __name__ == '__main__':
@@ -26,11 +26,22 @@ if __name__ == '__main__':
 	dirs = False
 	remotes = False
 	commands = False
+	user = False
+
+	# checks environment variables for username, system portable
+	username = getpass.getuser()	
 
 	inotify = Inotify()
 
+	# we don't need to consider name of script as an arg
 	sys.argv.pop(0)
 	
+	if len(sys.argv) == 0:
+		print __doc__
+		quit()
+
+	# sort of a switching system for handling arguments
+	# it may not be terribly efficient, but it makes adding arguments easy as pie
 	for arg in sys.argv:
 		if arg == "-h":
 			print __doc__
@@ -39,14 +50,22 @@ if __name__ == '__main__':
 			remotes = False
 			dirs = False
 			commands = True
+			user = False
 		elif arg == '-d':
 			remotes = False
 			dirs = True
 			commands = False
+			user = False
 		elif arg == '-r':
 			remotes = True
 			dirs = False
 			commands = False
+			user = False
+		elif arg == '-u':
+			remotes = False
+            dirs = False
+            commands = False
+            user = True
 		elif remotes:
 			remoteList.append(arg)
 		elif dirs:
@@ -54,6 +73,8 @@ if __name__ == '__main__':
 			dirList.append(arg)
 		elif commands:
 			commandList.append(arg)
+		elif user:
+			username = arg
 
 	for d in dirList:
 		# 180 hex is mask for both file creation and file move
@@ -75,8 +96,8 @@ if __name__ == '__main__':
 					' ' + dirn + '/.'
 
 				if (len(commandList) > 0):
-					sys.stdout.write('%s %s\n' % (' '.join(commandList),path))
-					os.system('%s %s' % (' '.join(commandList),path))
+					sys.stdout.write('%s@%s %s\n' % (username,' '.join(commandList),path))
+					os.system('%s@%s %s' % (username,' '.join(commandList),path))
 					break
 
 				else:
